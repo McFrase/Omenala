43ef2a12395efcb2fb03cce32d2484d665218ab3
Ebhota Fopefoluwa Sanchez <mc2frase@gmail.com>
2021-11-18 09:28:01 +0100

    First Commit

---
+++ js/omenala.js
@@ -0,0 +1,9 @@
+var typed = new Typed('.typed-words', {
+    strings: ["Hey there,", "Welcome", "How may we help you?"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 4000,
+    startDelay: 1000,
+    loop: true,
+    showCursor: true,
+    });
